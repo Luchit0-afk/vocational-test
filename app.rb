284309cd39ec44749478bacdb073639a29da5757
Luchit0-afk <lucianopalacio91@gmail.com>
2021-05-13 13:56:19 -0300

    Add the careers and questions tables

--- app.rb
+++ app.rb
@@ -10,6 +10,32 @@ class App < Sinatra::Base
    erb :hello_template
   end
 
+  post "/careers" do
+    data = JSON.parse request.body.read
+    career = Career.new(name: data['name'])
+
+    if career.save
+      [201, {'Location' => "careers/#{career.id}" },'CREATED']
+    else
+      [500,{},'Internal Server Error']
+    end
+  end
+
+  post "/posts" do
+    request.body.rewind  # in case someone already read it
+    data = JSON.parse request.body.read
+    post = Post.new(description: data['desc'])
+    if post.save
+      [201, { 'Location' => "posts/#{post.id}" }, 'CREATED']
+    else
+      [500, {}, 'Internal Server Error']
+    end
+  end
+
+  get '/careers' do
+    Career.all.map { |c| c.name}
+  end
+
   post "/posts" do
     request.body.rewind  # in case someone already read it
     data = JSON.parse request.body.read
