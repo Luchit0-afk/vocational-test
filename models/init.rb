efa74194831700227848783ce6aaaf247da43774
Alan <alan_arce97@hotmail.com>
2021-05-19 18:18:45 -0300

    agregue las tablas en init

--- models/init.rb
+++ models/init.rb
@@ -1,2 +1,7 @@
 require_relative 'post'
 require_relative 'career'
+require_relative 'survey'
+require_relative 'question'
+require_relative 'outcome'
+require_relative 'response'
+require_relative 'choice'
