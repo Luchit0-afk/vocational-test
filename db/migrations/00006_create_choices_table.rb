8bd134e8095707cb537e2b2f8b6c8397f98664a0
Alan <alan_arce97@hotmail.com>
2021-05-19 11:16:19 -0300

    cree tabla choices

---
+++ db/migrations/00006_create_choices_table.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    create_table(:choices) do
+      primary_key   :id
+      String        :text
+      foreign_key   :question_id,    :questions
+      DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
+      DateTime      :updated_at,   default: Sequel::CURRENT_TIMESTAMP
+    end
+  end
+
+  down do
+    drop_table(:choices)
+  end
+end
