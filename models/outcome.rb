1cd925c06e01d9c2333d975e037439d2a8349633
EAMolina500 <molinaenzoagustin@gmail.com>
2021-05-20 19:04:58 -0300

    associations

--- models/outcome.rb
+++ models/outcome.rb
@@ -1,2 +1,4 @@
 class Outcome < Sequel::Model
+	many_to_one :Career 
+	many_to_one :Choice
 end
--- models/response.rb
+++ models/response.rb
@@ -1,2 +1,5 @@
 class Response < Sequel::Model
+	many_to_one :Survey 
+	many_to_one :Question
+	one_to_one :Choice
 end
